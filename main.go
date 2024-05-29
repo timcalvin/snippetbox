456570cd413a145e4a83de0dadb4de68820f6be1
Tim Bryant <timcalvin@gmail.com>
2024-05-28 22:36:22 -0400

    HTTP Headers
    - customize create snippet http headers

--- main.go
+++ main.go
@@ -16,7 +16,7 @@ func home(w http.ResponseWriter, r *http.Request) {
 		http.NotFound(w, r)
 		return
 	}
-	
+
 	w.Write([]byte("Hello from Snippetbox"))
 }
 
@@ -27,6 +27,24 @@ func snippetView(w http.ResponseWriter, r *http.Request) {
 
 // Add a snippetCreate handler function.
 func snippetCreate(w http.ResponseWriter, r *http.Request) {
+	// Use r.Method to check whether the request is using POST or not.
+	if r.Method != http.MethodPost {
+		// If it's not,use the Header().Set() method to add an 'Allow: POST' header to the
+		// repsonse header map. The first parameter is the header name, and
+		// the second parameter is the header value.
+		w.Header().Set("Allow", http.MethodPost)
+		// Use the w.WriteHeader() method to send a 405 status
+		// code and the w.Write() method to write a "Method Not Allowed"
+		// response body. We then return from the function so that the
+		// subsequent code is not executed.
+		// w.WriteHeader(405)
+		// w.Write([]byte("Method Not Allowed"))
+		// or use http.Error() which calls both behind-the-scenes
+		http.Error(w, "Method Not Allowed", http.StatusMethodNotAllowed)
+
+		return
+	}
+
 	w.Write([]byte("Create a new snippet..."))
 }
 
